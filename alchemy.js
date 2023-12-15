c50c18087ffddf788dbae9ee2c30ef8edbd0de4f
HogyeJozsef <hogyejozsef@gmail.com>
2023-12-15 18:44:26 +0100

    V1.1

--- alchemy.js
+++ alchemy.js
@@ -1,9 +1,8 @@
 import { Alchemy, Network } from 'alchemy-sdk';
 
-// Optional config object, but defaults to the API key 'demo' and Network 'eth-mainnet'.
 const settings = {
-  apiKey: 'IwGO2kIZXDwf2wLfECr4W6YYa7-PA9cU', // Replace with your Alchemy API key.
-  network: Network.ETH_MAINNET // Replace with your network.
+  apiKey: '', 
+  network: Network.ETH_MAINNET 
 };
 
 const alchemy = new Alchemy(settings);
